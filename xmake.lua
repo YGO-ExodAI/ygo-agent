bab595968eaf7cf1bb86d02122e417acf6d87bba
Joe Darling <joedarling7@gmail.com>
2026-04-13 23:01:56 -0700

    xmake: remove edopro-core dep, add lua package for ygopro_ygoenv
    edopro-core fails to compile on GCC 11 with "template with C linkage"
    from /usr/include/c++/11/bits/allocated_ptr.h, caused by STL includes
    inside an extern "C" block in interpreter.h. Since only ygopro_ygoenv
    is built here and it doesn't depend on edopro-core, drop the require.
    Adds "lua" to add_requires + ygopro_ygoenv's add_packages for the
    build to resolve.

--- xmake.lua
+++ xmake.lua
@@ -2,10 +2,13 @@ add_rules("mode.debug", "mode.release")
 
 add_repositories("my-repo repo")
 
+-- NOTE: edopro-core dep temporarily removed; it fails to build on this system
+-- ("template with C linkage" from /usr/include/c++/11/bits/allocated_ptr.h).
+-- We only build ygopro_ygoenv here, which doesn't use edopro-core.
 add_requires(
-    "ygopro-core 0.0.2", "edopro-core", "pybind11 2.13.*", "fmt 10.2.*", "glog 0.6.0",
+    "ygopro-core 0.0.2", "pybind11 2.13.*", "fmt 10.2.*", "glog 0.6.0",
     "sqlite3 3.43.0+200", "concurrentqueue 1.0.4", "unordered_dense 4.4.*",
-    "sqlitecpp 3.2.1")
+    "sqlitecpp 3.2.1", "lua")
 
 
 target("ygopro0_ygoenv")
@@ -29,7 +32,7 @@ target("ygopro0_ygoenv")
 target("ygopro_ygoenv")
     add_rules("python.library")
     add_files("ygoenv/ygoenv/ygopro/*.cpp")
-    add_packages("pybind11", "fmt", "glog", "concurrentqueue", "sqlitecpp", "unordered_dense", "ygopro-core")
+    add_packages("pybind11", "fmt", "glog", "concurrentqueue", "sqlitecpp", "unordered_dense", "ygopro-core", "lua")
     set_languages("c++17")
     if is_mode("release") then
         set_policy("build.optimization.lto", true)
